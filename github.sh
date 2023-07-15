cad114c7c49a69e70910982f36d5f6cd7068c2d3
ArifzynXD <arifzynxd@gmail.com>
2023-07-15 11:57:22 +0700

    new update

---
+++ github.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+git config --global user.email "arifzynxd@gmail.com"
+git config --global user.name "ArifzynXD"
+git init
+git config --global --add safe.directory /storage/emulated/0/api
+git add .
+git commit -m "new update"
+git remote add origin https://github.com/Arifzxa19/my-api
